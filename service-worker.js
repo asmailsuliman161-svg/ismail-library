65aee2f743f83783288661671418c24db0b67973
اسماعيل سليمان حسن سليمان <asmailsuliman161@gmail.com>
2026-03-23 07:47:53 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,38 @@
+const CACHE_NAME = "ismail-library-v1";
 
+// الملفات الأساسية
+const urlsToCache = [
+  "/",
+  "index.html",
+  "style.css",
+  "books.json",
+  "logo.png",
+  "images/ismail_photo.jpg",
+  "images/book_default.png"
+];
+
+// ===== تثبيت Service Worker =====
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log("تم حفظ الملفات");
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// ===== تشغيله =====
+self.addEventListener("activate", event => {
+  console.log("Service Worker مفعل");
+});
+
+// ===== جلب الملفات (بدون إنترنت) =====
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
